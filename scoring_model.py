7d6cb7e7fc10e511f26afe626511d06a3398cc26
chelloveck <geminat@gmail.com>
2019-01-30 19:46:37 +0200

    1.0081 plain score modeling

--- scoring_model.py
+++ scoring_model.py
@@ -2,12 +2,12 @@ import datetime
 import matplotlib.pyplot as plt
 
 def feb_plain_model():
-    dates = [datetime.date(2019, 2, 1) + datetime.timedelta(i) for i in range(1, 30)]
+    dates = [datetime.date(2019, 2, 1) + datetime.timedelta(i) for i in range(30)]
     parts = [dates[:i] for i in range(1, len(dates))]
     hols = [len([a for a in v if a.weekday() in (5, 6)]) for v in parts]
     work = [len([a for a in v if a.weekday() not in (5, 6)]) for v in parts]
     days = [len(part) for part in parts]
-    div = [round((((a+b)/(a)) - (a+b)/(b))/b, 2) for a, b in zip(hols, days)]
+    div = [round(a/b, 2)for a, b in zip(hols, days)]
     div_zoom = [a * 30 for a in div]
 
     fig = plt.figure(figsize=(12, 8), dpi = 100)
@@ -17,8 +17,8 @@ def feb_plain_model():
     plt.plot(days, hols, 'b-', label='holidays', linewidth=1)
     plt.plot(days, work, 'r-', label='working days', linewidth=1)
 
-    # ax.set_yticks(list(range(0, 30, 1)))
-    # ax.set_xticks(list(range(0, 30, 1)))
+    ax.set_yticks(list(range(0, 30, 1)))
+    ax.set_xticks(list(range(0, 30, 1)))
 
 
     ax.grid(which='major', alpha=1)
