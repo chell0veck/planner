a934a1e1152c7137e5f2661ded328cd8bb36e018
chell0veck <geminat@gmail.com>
2019-05-12 00:06:19 +0300

    major update

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ def default_model():
 
     events = songkick.load_events()
 
-    for event in events:
-        if event.country == 'Russia':
+    for event in sorted(events, key=lambda e: e.date):
+        if event.country not in static.SKIP_CTRY\
+                and event.country == 'Ireland':
             print(event, event.date.strftime("%A"))
 
 
