cc50870df3e6126a313cade4cb50ebc879aeae8c
chell0veck <geminat@gmail.com>
2019-05-11 23:42:33 +0300

    major update

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import datetime
-
 import songkick
 import static
 
-# songkick.dump_events()
 
-events = songkick.load_events()
+def default_model():
 
+    events = songkick.load_events()
 
-for event in events:
-    if event.country not in static.SKIP_CTRY\
-            and event.type == 'Concert'\
-            and event.date.weekday() in (4, 5, 6):
-        print(event, event.date.strftime("%A"))
+    for event in events:
+        if event.country == 'Russia':
+            print(event, event.date.strftime("%A"))
 
 
+default_model()
