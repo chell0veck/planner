e5351af4f9caf16e1f0c880395a399c9dbcc095a
chell0veck <geminat@gmail.com>
2019-05-12 18:20:47 +0300

    major update

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import songkick
 import static
 import calendarific
-import pickle
-
-
-song_api = songkick.API()
 
 
 def default_model():
 
-    events = song_api.load_cache()
+    events = songkick.load_events()
 
     for event in sorted(events, key=lambda e: e.date):
         if event.country not in static.SKIP_CTRY\
                 and event.date.strftime("%m") in ('10', '11'):
             print(event, event.date.strftime("%A"), event.venue, event.type)
-
-
-song_api.dump_cache()
-
-default_model()
-
--- planner/src/calendarific.py
+++ planner/src/calendarific.py
@@ -1,24 +1,42 @@
-import json
+import datetime
 import requests
+import pickle
+import os
+
+from pathlib import Path
 
 
-class V2:
-    api_key = None
+api_key = open(os.path.join(Path(__file__).parents[1], 'resources', '.calendarific_api_key'), 'r').read()
+params = {'api_key': api_key, 'country': 'ua', 'year': datetime.datetime.today().year}
+cache = os.path.join(Path(__file__).parents[1], 'resources', 'holidays.pickle')
+url = 'https://calendarific.com/api/v2/holidays'
 
-    def __init__(self, api_key):
-        self.api_key = api_key
 
-    def holidays(self, parameters):
-        url = 'https://calendarific.com/api/v2/holidays?'
+def get_holidays():
+    res = requests.get(url=url, params=params)
+    out = []
 
-        if parameters.has_key('api_key') is False:
-            parameters['api_key'] = self.api_key
+    if res.status_code == 200:
+        holidays = res.json()['response']['holidays']
 
-        response = requests.get(url, params=parameters)
-        data = json.loads(response.text)
+        for holiday in holidays:
+            if 'National holiday' in holiday['type']:
+                _date = datetime.datetime.strptime(holiday['date']['iso'], '%Y-%m-%d')
 
-        if response.status_code != 200:
-            if data.has_key('error') is False:
-                data['error'] = 'Unknown error.'
+                if _date.weekday() not in (5, 6):
+                    out.append(_date)
+    return out
 
-        return data
+
+def dump_holidays():
+    """ Dump holidays """
+    holidays = get_holidays()
+
+    with open(cache, 'wb') as f:
+        pickle.dump(holidays, f)
+
+
+def load_holidays():
+    """ Load holidays from the file"""
+    with open(cache, 'rb') as f:
+        return pickle.load(f)
