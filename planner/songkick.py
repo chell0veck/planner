9ab89116ee96a2d2fe516e4e618a41561478ea57
chelloveck <geminat@gmail.com>
2019-05-16 18:37:26 +0300

    minor update

--- planner/songkick.py
+++ planner/songkick.py
@@ -23,25 +23,28 @@ API_KEY = open('.songkick_api_key', 'r').read()
 CACHE = os.path.join(Path(__file__).parents[0], '_static_events.json')
 
 
-def get_events_by_id(artist_id):
-    url = f'https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={API_KEY}'
-    res = requests.get(url).json()
-
-    status_ok = res['resultsPage']['status'] == 'ok'
-    events_exists = 'event' in res['resultsPage']['results']
-
-    if status_ok and events_exists:
-        events = res['resultsPage']['results']['event']
-
-        return events
-
-
-def get_events_by_ids(artists_ids):
+def get_events_by_ids(artists):
     out = []
 
-    for artist_id in set(artists_ids):
-        events = get_events_by_id(artist_id)
-        out.append(events)
+    for artist in set(artists):
+        url = f'https://api.songkick.com/api/3.0/artists/{artist}/calendar.json?apikey={API_KEY}'
+        res = requests.get(url).json()
+
+        status_ok = res['resultsPage']['status'] == 'ok'
+        events_exists = 'event' in res['resultsPage']['results']
+
+        if status_ok and events_exists:
+            events = res['resultsPage']['results']['event']
+            out.append(events)
 
     return out
+
+
+def get_events_by_name(name):
+    """ Get events by dict {'artist name': artist_id}"""
+    pass
+
+arts = [521019, 201140]
+d = {"Mono": 201140, 'smth': 222, 'abot': 33}
+
 
