17afaa2cdb1aa27952bfcc08491af6f35c93257a
chell0veck <geminat@gmail.com>
2019-05-17 01:50:29 +0300

    minor

--- planner/main.py
+++ planner/main.py
@@ -1,21 +1,18 @@
 import timetable
 import songkick
-import json
-
-
-from tools import Event, Date, wrap_events
+import tools
 
 
 def default_model():
     result = []
     raw_events = songkick.load_events()
-    fmt_events = wrap_events(raw_events)
+    fmt_events = tools.wrap_events(raw_events)
     nonwork = timetable.get_nonwork()
 
     for event in fmt_events:
         if event.date in nonwork:
-            result.append(Date(event.date, event, True))
-        result.append(Date(event.date, event, False))
+            result.append(tools.Date(event.date, event, True))
+        result.append(tools.Date(event.date, event, False))
 
     return result
 
