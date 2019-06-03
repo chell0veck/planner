8018abccf75771b1e6bc9cd9552fd0ce14ce2155
chelloveck <geminat@gmail.com>
2019-06-03 16:54:56 +0300

    adjust frame

--- planner/date_utils.py
+++ planner/date_utils.py
@@ -5,38 +5,10 @@ import holidays
 from utils import Day
 
 
-def get_non_work(year=datetime.datetime.today().year):
-    """
-    To be defined
-    :param year:
-    :return:
-    """
-
-    _holidays = [dt for dt in holidays.UA(years=year)]
-    first_day = datetime.date(year, 1, 1)
-    weekdays = [(first_day + datetime.timedelta(i)) for i in range(370)
-                if (first_day + datetime.timedelta(i)).weekday() in (5, 6)
-                and (first_day + datetime.timedelta(i)).year == year]
-
-    non_working_days = _holidays + weekdays
-
-    return sorted(non_working_days)
-
-
-def generate_year_dates(days):
-    result = days
-    start = datetime.date(2019, 1, 1)
-    days_with_events = [day.date for day in days]
-    all_days = [start + datetime.timedelta(days=n) for n in range(365)]
-    days_without_events = [Day(day, False, False) for day in all_days if day not in days_with_events]
-    result.extend(days_without_events)
-
-    return result
-
-
-def generate_frames(start_day=datetime.date.today(), end_day=None, max_range=20):
+def generate_frames(start_day=None, end_day=None, max_range=20):
     result = []
-    end_day = end_day if end_day is not None else datetime.date(start_day.year, 12, 31)
+    start_day = start_day if start_day else datetime.date.today()
+    end_day = end_day if end_day is not None else datetime.date(start_day.year + 1, 1, 1)
     diff = (end_day-start_day-datetime.timedelta(days=max_range)).days + 2
 
     for start in range(diff):
@@ -64,21 +36,17 @@ def is_non_working(date: 'datetime.date') -> 'bool':
     return False
 
 
-def adjust_frame(start: 'datetime.date', end: 'datetime.date') -> 'tuple':
+def adjust_frame(frame: 'tuple') -> 'tuple':
     """
     Extend given frame with nonworking days if such border with given start or end
        adjust_frame(datetime.date(2019, 6, 24), datetime.date(2019, 6, 27)) -> (datetime.date(2019, 6, 22), datetime.date(2019, 6, 30)):
 
-    :param start: Start day of the frame
-                June 24 2019 is Monday -> new start day is Saturday June 22 2019
-    :param end: End day of the frame
-                June 27 2019 is Thursday but June 28 is public holiday -> new end day is Sunday June 30 2019
+    :param frame: Date frame (tuple of datetime.date objects)
+
     :return: Adjusted frame
 
     """
-
-    new_start = start
-    new_end = end
+    new_start, new_end = frame
 
     while is_non_working(new_start - datetime.timedelta(days=1)):
         new_start -= datetime.timedelta(days=1)
